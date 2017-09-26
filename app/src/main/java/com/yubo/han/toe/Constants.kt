04c54d34648c5f93bc3b7da2344ab1cb697d31bf
hanwang107 <wanghan107@gmail.com>
2017-09-26 02:02:07 -0400

    add Constants class

---
+++ app/src/main/java/com/yubo/han/toe/Constants.kt
@@ -0,0 +1,11 @@
+package com.yubo.han.toe
+
+/**
+ * Created by han on 9/26/17.
+ */
+object Constants {
+    // Yelp search
+    val term = "landmarks"
+    val YELP_SEARCH_URL = "https://api.yelp.com/v3/businesses/search"
+    val YELP_SEARCH_TOKEN = "Bearer 7V8pfQGeJaRcSJHwizHuMJ_OMHuITa4DrO7ZXpTDCgv1CclbSkctHq0qtMi-jG5PkxIQ8ZoZuy_G0v338DFGw9qZMHNf4TQmugebOLIukFW8lL1uJTRzcySOMkTHWXYx"
+}
