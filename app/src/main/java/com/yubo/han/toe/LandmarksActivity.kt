4d266eab195199f77401f104040cc853bfb57d07
hanwang107 <wanghan107@gmail.com>
2017-09-26 03:19:07 -0400

    query landmarks from yelp api

--- app/src/main/java/com/yubo/han/toe/LandmarksActivity.kt
+++ app/src/main/java/com/yubo/han/toe/LandmarksActivity.kt
@@ -2,11 +2,34 @@ package com.yubo.han.toe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+import com.google.gson.JsonObject
+import com.koushikdutta.ion.Ion
+import com.yubo.han.toe.Services.FetchLandmarksManager
+import org.jetbrains.anko.toast
 
 class LandmarksActivity : AppCompatActivity() {
+
+    lateinit var fetchLandmarksManager: FetchLandmarksManager
+
+    private val LOG_TAG = "LandmarksActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landmarks)
+
+//        val json = queryYelpForLandMarks("foggybottom")
+//        toast("${json}")
+
+        loadYelp()
     }
+
+    fun loadYelp() {
+        fetchLandmarksManager = FetchLandmarksManager(this)
+        //To do
+        fetchLandmarksManager.queryYelpForLandMarks("foggybottom")
+
+    }
+    
 }
--- app/src/main/java/com/yubo/han/toe/Services/FetchLandmarksManager.kt
+++ app/src/main/java/com/yubo/han/toe/Services/FetchLandmarksManager.kt
@@ -1,7 +1,80 @@
 package com.yubo.han.toe.Services
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.util.Log
+import com.google.gson.JsonObject
+import com.koushikdutta.ion.Ion
+import com.yubo.han.toe.Constants
+import org.jetbrains.anko.toast
+
 /**
  * Created by han on 9/25/17.
  */
-class FetchLandmarksManager {
+class FetchLandmarksManager(val context: Context) {
+    private val LOG_TAG = "FetchLandmarksManager"
+//    var imageSearchCompletionListener: ImageSearchCompletionListener? = null
+//
+//    interface ImageSearchCompletionListener {
+//        fun imageLoaded()
+//        fun imageNotLoaded()
+//    }
+
+
+
+    //Query landmarks list from Yelp Api
+    fun queryYelpForLandMarks(address: String) {
+        Ion.with(context).load(Constants.YELP_SEARCH_URL)
+                .addHeader("Authorization", Constants.YELP_SEARCH_TOKEN)
+                .addQuery("term", Constants.term)
+                .addQuery("location", address)
+                .asJsonObject()
+                .setCallback { error, result ->
+                    error?.let {
+                        Log.e(LOG_TAG, it.message)
+                    }
+
+                    result?.let {
+                        val json = parseLandmarksFromJSON(it)
+
+                        if (json != null) {
+                            Log.e(LOG_TAG, "${json}")
+                        }else {
+                            Log.e(LOG_TAG, "cannot get parsed json")
+                        }
+
+                    }
+                }
+
+    }
+
+
+    // Get landmarks list from Jsonobject
+    fun parseLandmarksFromJSON(jsonobject: JsonObject): String? {
+
+        val landmarksResults = jsonobject.getAsJsonArray("businesses").get(1).toString()
+        return landmarksResults
+
+
+
+    }
+
+
+
+
+
+
+//    fun queryYelpForLandMarks(address: String): JsonObject? {
+//        try {
+//            return Ion.with(context).load(Constants.YELP_SEARCH_URL)
+//                    .addHeader("Authorization", Constants.YELP_SEARCH_TOKEN)
+//                    .addQuery("term", Constants.term)
+//                    .addQuery("location", address)
+//                    .asJsonObject().get()
+//        } catch (e: Exception) {
+//            Log.e(LOG_TAG, e.message)
+//            return null
+//        }
+//    }
+
 }
