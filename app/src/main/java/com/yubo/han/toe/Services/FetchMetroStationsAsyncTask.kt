d52e6464791bec619366347969ead757d59ae58f
Yurockkk <r00631026@ntu.edu.tw>
2017-09-25 23:10:36 -0400

    Add FetchMetroStationsManager.kt & delete FetchMetroStationsAsyncTask.kt

--- app/src/main/java/com/yubo/han/toe/Services/FetchMetroStationsAsyncTask.kt
+++
@@ -1,7 +0,0 @@
-package com.yubo.han.toe.Services
-
-/**
- * Created by han on 9/25/17.
- */
-class FetchMetroStationsAsyncTask {
-}
---
+++ app/src/main/java/com/yubo/han/toe/Services/FetchMetroStationsManager.kt
@@ -0,0 +1,7 @@
+package com.yubo.han.toe.Services
+
+/**
+ * Created by yubo on 9/25/17.
+ */
+class FetchMetroStationsManager {
+}
