c2aef7ff42a80abb80c0365bb7d2f480519d0c31
Yurockkk <r00631026@ntu.edu.tw>
2017-09-26 21:50:02 -0400

    Implement query WMATA for all stations & Add mFetchMetroStationsManager in MetroStationsActivity

--- app/src/main/java/com/yubo/han/toe/MetroStationsActivity.kt
+++ app/src/main/java/com/yubo/han/toe/MetroStationsActivity.kt
@@ -2,11 +2,22 @@ package com.yubo.han.toe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.yubo.han.toe.Services.FetchMetroStationsManager
 
 class MetroStationsActivity : AppCompatActivity() {
+
+    val LOG_TAG = "MetroStationsActivity"
+    lateinit var mFetchMetroStationsManager: FetchMetroStationsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_metro_stations)
+
+        loapMetroStations()
+    }
+
+    private fun loapMetroStations() {
+        mFetchMetroStationsManager = FetchMetroStationsManager(this)
+        mFetchMetroStationsManager.queryWMATAForAllStations()
     }
 }
--- app/src/main/java/com/yubo/han/toe/Services/FetchMetroStationsManager.kt
+++ app/src/main/java/com/yubo/han/toe/Services/FetchMetroStationsManager.kt
@@ -1,7 +1,60 @@
 package com.yubo.han.toe.Services
 
+import android.content.Context
+import android.util.Log
+import com.google.gson.JsonObject
+import com.koushikdutta.ion.Ion
+import com.yubo.han.toe.Constants
+
 /**
  * Created by yubo on 9/25/17.
  */
-class FetchMetroStationsManager {
+class FetchMetroStationsManager(val context: Context) {
+
+    private val LOG_TAG = "FetchMetroStationsManager"
+
+    fun queryWMATAForAllStations(){
+
+        Ion.with(context)
+                .load(Constants.METRO_ALLSTATION_SEARCH_URL)
+                .asJsonObject()
+                .setCallback { error, result ->
+                    error?.let {
+                        Log.e(LOG_TAG, it.message)
+                    }
+
+                    result?.let {
+                        //Log.i(LOG_TAG, result.toString())
+                        val json = parseStationsFromJSON(it)
+
+                        if (json != null) {
+                            Log.e(LOG_TAG, "${json}")
+                        }else {
+                            Log.e(LOG_TAG, "cannot get parsed json")
+                        }
+
+                    }
+                }
+
+    }
+
+    // Get stations list from Jsonobject
+    fun parseStationsFromJSON(jsonobject: JsonObject): ArrayList<String>? {
+
+        var stations = arrayListOf<String>()
+
+        val stationsResults = jsonobject.getAsJsonArray("Stations")
+        if (stationsResults != null && stationsResults.size() > 0) {
+            for (i in 0..stationsResults.size() - 1) {
+                var landmarksResult = stationsResults.get(i).toString()
+                stations.add(landmarksResult)
+            }
+
+            return stations
+        }
+
+        return null
+
+    }
+
 }
