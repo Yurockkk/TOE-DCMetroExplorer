ffdc063f8cc3169b5583e6c2d71e30ccad44b215
hanwang107 <wanghan107@gmail.com>
2017-09-27 01:08:39 -0400

    Add Landmarks data class and correct a typo in menuactivity

---
+++ app/src/main/java/com/yubo/han/toe/model/Landmarks.kt
@@ -0,0 +1,7 @@
+package com.yubo.han.toe.model
+
+/**
+ * Created by han on 9/27/17.
+ */
+data class Landmarks (val name: String, val imageUrl: String, val latitude: Float,
+                      val longitude: Float)
