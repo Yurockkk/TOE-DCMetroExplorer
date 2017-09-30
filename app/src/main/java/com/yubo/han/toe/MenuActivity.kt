32dda2e2c315bc9d827e882322cbf232d2254c8f
Yurockkk <r00631026@ntu.edu.tw>
2017-09-30 18:39:10 -0400

    Implement LocationDetector.kt

--- app/src/main/java/com/yubo/han/toe/MenuActivity.kt
+++ app/src/main/java/com/yubo/han/toe/MenuActivity.kt
@@ -3,6 +3,7 @@ package com.yubo.han.toe
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
@@ -10,28 +11,53 @@ import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_menu.*
-import org.jetbrains.anko.toast
-import com.google.android.gms.location.LocationServices
+import com.yubo.han.toe.Services.LocationDetector
+
 //import com.google.android.gms.location.FusedLocationProviderClient
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), LocationDetector.LocationDetectCompletedListener {
+
 
     val LOG_TAG = "MenuActivity"
     val MY_PERMISSIONS_REQUEST_FINE_LOCATION = 666
     //lateinit var mFusedLocationClient: Any
+    lateinit var mLocationDetector: LocationDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        mLocationDetector = LocationDetector(this)
+        //register itself to metroStationsCompletedListener
+        mLocationDetector.locationDetectCompletedListener = this
+
         initUI()
+
+        //check location permission, if granded -> get location
+        checkLocationPermission()
     }
 
     fun initUI(){
         near_station_button.setOnClickListener(View.OnClickListener {
 
             //check location permission, if granded -> get location
-            checkLocationPermission()
+//            checkLocationPermission()
+
+            // Assume thisActivity is the current activity
+            val permissionCheck = ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+            Log.i(LOG_TAG,"permissionCheck: $permissionCheck")
+
+            if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+                //get location permission, start to detect location
+                mLocationDetector.getDeviceLastLocation()
+
+            }else{
+                //TODO: maybe popup a dialog to tell user we need the permission to use 'near station' function?
+                Log.i(LOG_TAG, "ask user for location permission!")
+                checkLocationPermission()
+            }
+
 
         })
 
@@ -61,7 +87,7 @@ class MenuActivity : AppCompatActivity() {
                 Manifest.permission.ACCESS_FINE_LOCATION)
 
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_CONTACTS)
+                Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
             // Should we show an explanation?
@@ -89,29 +115,25 @@ class MenuActivity : AppCompatActivity() {
 
         when(requestCode){
 
-            MY_PERMISSIONS_REQUEST_FINE_LOCATION -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){ getDeviceLastLocation() }
-
+            MY_PERMISSIONS_REQUEST_FINE_LOCATION -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // enable user to click 'near station btn'
+            }else{
+                //TODO: maybe popup a dialog to tell user we need the permission to use 'near station' function?
+            }
         }
     }
 
-    fun getDeviceLastLocation(){
 
-        // create an instance of the Fused Location Provider Client
-        var mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        try{
-            mFusedLocationClient.lastLocation
-                    .addOnSuccessListener(this) { location ->
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            Log.i(LOG_TAG,location.toString())
-                            toast(location.toString())
-                        }
-                    }
-        }catch (e:SecurityException){
+    //LocationDetectCompletedListener implementation
 
-            Log.e(LOG_TAG,e.toString())
-        }
+    override fun locationDetected(location: Location) {
 
+        Log.i(LOG_TAG,"location detected")
+
+    }
+
+    override fun locationNotDetected() {
+
+        Log.i(LOG_TAG, "location not detected")
     }
 }
--- app/src/main/java/com/yubo/han/toe/Services/LocationDetector.kt
+++ app/src/main/java/com/yubo/han/toe/Services/LocationDetector.kt
@@ -1,7 +1,45 @@
 package com.yubo.han.toe.Services
 
+import android.content.Context
+import android.location.Location
+import android.util.Log
+import com.google.android.gms.location.LocationServices
+
 /**
  * Created by han on 9/25/17.
  */
-class LocationDetector {
+class LocationDetector(val context: Context) {
+
+    private val LOG_TAG = "LocationDetector"
+
+    var locationDetectCompletedListener: LocationDetectCompletedListener? = null
+
+    interface LocationDetectCompletedListener{
+
+        fun locationDetected(location: Location)
+        fun locationNotDetected()
+    }
+
+
+    fun getDeviceLastLocation(){
+
+        // create an instance of the Fused Location Provider Client
+        var mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        try{
+            mFusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                 //Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            Log.i(LOG_TAG,location.toString())
+                            locationDetectCompletedListener?.locationDetected(location)
+                        }else{
+                            locationDetectCompletedListener?.locationNotDetected()
+                        }
+            }
+        }catch (e:SecurityException){
+
+            Log.e(LOG_TAG,e.toString())
+        }
+
+    }
 }
