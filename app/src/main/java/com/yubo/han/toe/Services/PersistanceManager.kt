1c330a40d6d6a9fd97b6156fac98f116eb1a4cbd
Yurockkk <r00631026@ntu.edu.tw>
2017-10-03 21:57:10 -0400

    Implement PersistanceManager(WIP)

--- app/src/main/java/com/yubo/han/toe/Services/PersistanceManager.kt
+++ app/src/main/java/com/yubo/han/toe/Services/PersistanceManager.kt
@@ -1,7 +1,43 @@
 package com.yubo.han.toe.Services
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.yubo.han.toe.Constants
+import com.yubo.han.toe.model.Landmarks
+
 /**
  * Created by han on 9/25/17.
  */
-class PersistanceManager {
+class PersistanceManager(context: Context) {
+    val sharedPreferences: SharedPreferences
+
+    init {
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    }
+
+    fun saveLandmark(landmark: Landmarks){
+
+        val landmarks = fetchLandmarks().toMutableList()
+        landmarks.add(landmark)
+
+        val editor = sharedPreferences.edit()
+        editor.putString(Constants.LANDMARKS_PREF_KEY,Gson().toJson(landmarks))
+
+        editor.apply()
+    }
+
+    fun fetchLandmarks(): List<Landmarks>{
+        var landmarksJson = sharedPreferences.getString(Constants.LANDMARKS_PREF_KEY,null)
+        if(landmarksJson == null){
+            return arrayListOf<Landmarks>()
+        }
+        else{
+            val landmarksType = object : TypeToken<MutableList<Landmarks>>() {}.type
+            return Gson().fromJson(landmarksJson,landmarksType)
+        }
+    }
+
 }
